278c0ebb57c079d40e0ead5c4df51040266d5af1
Htut Lynn Aung <htutlynnaung.cs@gmail.com>
2021-01-20 20:08:10 +0630

    Download paths are replaced with true ones in download_models.py.

--- download_models.py
+++ download_models.py
@@ -8,13 +8,13 @@ dest = "checkpoints"
 
 # Create blob client instance
 client = BlobClient(connection_string=os.environ['AZURE_STORAGE_CONNECTION_STRING'],
-                    container_name='Downloadblob')
+                    container_name='downloadblob')
 
 # Download pytorch and ONNX model files for EfficientDet
 client.download(source="EfficientDet", dest=dest)
 
 # Download pytorch and ONNX model files for DeepSORT feature Extractor
-client.download(source="Extractor", dest=dest)
+client.download(source="extractor", dest=dest)
 
 # Download pytorch and ONNX model files for YOLOv4
-client.download(source="YOLOv4", dest=dest)
+client.download(source="digits", dest=dest)
